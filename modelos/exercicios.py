7af7dfaea749e3c306c510d5f8574f7d19ec9281
lremor <luisremor@hotmail.com>
2024-01-26 20:51:29 -0300

    classe - exercicios 1 ao 9

--- modelos/exercicios.py
+++ modelos/exercicios.py
@@ -1,15 +1,42 @@
 # 01. Classes
 
-#Exercicio 01
+# Exercicio atributos de uma classe
 
-class Musica:
+# class Musica:
+#     artista = ''
+#     nome = ''
+#     duracao = ''
+
+# musica = Musica()
+# musica.artista = 'Linkin Park'
+# musica.nome = 'In the end'
+# musica.duracao = '3:00'
+
+# print(f'banda: {musica.artista}\nnome: {musica.nome}\nduracao: {musica.duracao}')
+
+# Exercicio 01 ao 09
+
+class Restaurante:
     nome = ''
-    artista = ''
-    duracao = ''
+    categoria = ''
+    ativo = False
 
-musica = Musica()
-musica.nome = 'In the end'
-musica.artista = 'Linkin Park'
-musica.duracao = '3:00'
+restaurante_praca = Restaurante()
+restaurante_praca.nome = 'Bistrô'
+restaurante_praca.categoria = 'Italiana'
+categoria = Restaurante.categoria
 
-print(vars(musica))
+restaurante_pizza = Restaurante()
+restaurante_pizza.nome = 'Pizza Place'
+restaurante_pizza.categoria = 'Fast Food'
+restaurante_pizza.ativo = True
+
+if restaurante_praca.ativo == False:
+    print(f'restaurante: {restaurante_praca.nome}\ncategoria: {restaurante_praca.categoria}\nativo: não')
+else:
+    print(f'restaurante: {restaurante_praca.nome}\ncategoria: {restaurante_praca.categoria}\nativo: sim')
+
+if restaurante_pizza.ativo == False:
+    print(f'restaurante: {restaurante_pizza.nome}\ncategoria: {restaurante_pizza.categoria}\nativo: não')
+else:
+    print(f'restaurante: {restaurante_pizza.nome}\ncategoria: {restaurante_pizza.categoria}\nativo: sim')
