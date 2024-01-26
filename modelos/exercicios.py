5c582b710cedb2d87a632cac09a33715829e33d2
lremor <luisremor@hotmail.com>
2024-01-26 15:14:04 -0300

    oo-Sabor Express

---
+++ modelos/exercicios.py
@@ -0,0 +1,15 @@
+# 01. Classes
+
+#Exercicio 01
+
+class Musica:
+    nome = ''
+    artista = ''
+    duracao = ''
+
+musica = Musica()
+musica.nome = 'In the end'
+musica.artista = 'Linkin Park'
+musica.duracao = '3:00'
+
+print(vars(musica))
